f4337ab43deef9b6086b23eeb211dce0171d411a
Vallejos Jose Luis <joseluisvallejos1995@gmail.com>
2023-03-10 11:28:31 -0300

    test: :white_check_mark: Pass test

--- ejerciciosSimples/app.js
+++ ejerciciosSimples/app.js
@@ -2,14 +2,24 @@ console.log('Holaaa');
 
 function fizzbuzz (num){
 
+    const isZero = 0;
     const isDivisibleBy3 = num % 3 === 0;
     const isDivisibleBy5 = num % 5 === 0;
 
+    if(num === isZero) return 0;
     if (isDivisibleBy3 && isDivisibleBy5) return 'fizzbuzz';
     if (isDivisibleBy3) return 'fizz';
     if (isDivisibleBy5) return 'buzz';
 
     return num;
 }
+
+function print(num){
+    for (let index = 0; index < num; index++) {
+        console.log(`${index}: ${fizzbuzz(index)}`);
+    }
+}
+
+print(16);
 
 module.exports = fizzbuzz;
